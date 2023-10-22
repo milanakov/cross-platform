0d753ad5e1b7c9ea6eca9ab56ac645e88034c0d2
milanakov <71836456+milanakov@users.noreply.github.com>
2023-10-23 00:56:18 +0200

    Add files via upload

---
+++ lab4_crossplatform/Program_lab3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static ClassLibrary_lab3cross.Class1;
+
+class Program
+{
+    static void Main()
+    {
+        RunAlgorithm();
+    }
+}
